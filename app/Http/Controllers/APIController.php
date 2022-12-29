55e6cf940dd0dcf9ac2f9a647db98fe490436668
Arafat Anwar <programmer.arafatanwar@gmail.com>
2022-12-29 19:56:35 +0600

    API

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,441 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Models\User;
+use \App\Models\Sector;
+use \App\Models\Budget;
+use \App\Models\Entry;
+use DB;
+
+class APIController extends Controller
+{
+    public function checkUser($email, $password)
+    {
+        $user = User::where('email', $email)->first();
+        if(isset($user->id)){
+            if(\Hash::check($password, $user->password)){
+                return [
+                    'success' => true,
+                    'user' => $user
+                ];
+            }
+
+            return [
+                'success' => false,
+                'message' => "Password is Incorrect!"
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => "User not Found!"
+        ];
+    }
+
+    public function registration(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'name'           => 'required',
+            'email'      => 'required|unique:users',
+            'password'      => 'required|min:6',
+        ]);
+
+        if($validator->passes()){
+            $registration = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Your Ragistration is successful."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function sectors(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        return response()->json(Sector::where('user_id', $checkUser['user']->id)->get());
+    }
+
+    public function createSector(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'name'           => 'required',
+            'type'           => 'required',
+        ]);
+
+        if($validator->passes()){
+            Sector::updateOrCreate([
+                'user_id' => $checkUser['user']->id,
+                'name' => $request->name,
+                'type' => $request->type,
+            ], []);
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Sector Saved successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function updateSector(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'id'           => 'required',
+            'name'           => 'required',
+            'type'           => 'required',
+        ]);
+
+        if($validator->passes()){
+            Sector::updateOrCreate([
+                'id' => $request->id,
+                'user_id' => $checkUser['user']->id
+            ],[
+                'name' => $request->name,
+                'type' => $request->type,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Sector Updated successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function deleteSector(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'id'           => 'required',
+        ]);
+
+        if($validator->passes()){
+            Sector::where([
+                'id' => $request->id,
+                'user_id' => $checkUser['user']->id
+            ])->delete();
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Sector Deleted successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function budgets(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $budgets = Budget::with(['sector'])
+        ->whereHas('sector.user', function($query) use($checkUser){
+            return $query->where('user_id', $checkUser['user']->id);
+        })
+        ->when(isset($request->sector_id), function($query) use($request){
+            return $query->where('sector_id', $request->sector_id);
+        })
+        ->when(isset($request->year), function($query) use($request){
+            return $query->where('year', $request->year);
+        })
+        ->when(isset($request->month), function($query) use($request){
+            return $query->where('month', $request->month);
+        })
+        ->get();
+
+        return response()->json($budgets);
+    }
+
+    public function createBudget(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'sector_id' => 'required',
+            'year' => 'required',
+            'month' => 'required',
+            'budget' => 'required',
+            'remarks' => 'required',
+        ]);
+
+        if($validator->passes()){
+            Budget::create($request->all());
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Budget Created successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function updateBudget(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'id' => 'required',
+            'sector_id' => 'required',
+            'year' => 'required',
+            'month' => 'required',
+            'budget' => 'required',
+            'remarks' => 'required',
+        ]);
+
+        if($validator->passes()){
+            $budget = Budget::where('id', $request->id)->whereHas('sector.user', function($query) use($checkUser){
+                return $query->where('user_id', $checkUser['user']->id);
+            })->first();
+
+            $budget->fill($request->all())->save();
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Budget Updated successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function deleteBudget(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'id'           => 'required',
+        ]);
+
+        if($validator->passes()){
+            Budget::where('id', $request->id)->whereHas('sector.user', function($query) use($checkUser){
+                return $query->where('user_id', $checkUser['user']->id);
+            })->delete();
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Sector Deleted successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function entries(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $entries = Entry::with(['sector'])
+        ->whereHas('sector.user', function($query) use($checkUser){
+            return $query->where('user_id', $checkUser['user']->id);
+        })
+        ->when(isset($request->sector_id), function($query) use($request){
+            return $query->where('sector_id', $request->sector_id);
+        })
+        ->when(isset($request->from), function($query) use($request){
+            return $query->where('date', '>=', $request->from);
+        })
+        ->when(isset($request->to), function($query) use($request){
+            return $query->where('date', '<=', $request->to);
+        })
+        ->get();
+
+        return response()->json($entries);
+    }
+
+    public function createEntry(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'sector_id' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+            'title' => 'required',
+            'description' => 'required',
+            'amount' => 'required',
+        ]);
+
+        if($validator->passes()){
+            Entry::create($request->all());
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Entry Created successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function updateEntry(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'id' => 'required',
+            'sector_id' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+            'title' => 'required',
+            'description' => 'required',
+            'amount' => 'required',
+        ]);
+
+        if($validator->passes()){
+            $entry = Entry::where('id', $request->id)->whereHas('sector.user', function($query) use($checkUser){
+                return $query->where('user_id', $checkUser['user']->id);
+            })->first();
+
+            $entry->fill($request->all())->save();
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Entry Updated successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+
+    public function deleteEntry(Request $request)
+    {
+        $checkUser = $this->checkUser($request->email, $request->password);
+        if(!$checkUser['success']){
+            return response()->json([
+                'success' => false,
+                'errors' => $checkUser['message']
+            ]);
+        }
+
+        $validator = \Validator::make($request->all(), [
+            'id'           => 'required',
+        ]);
+
+        if($validator->passes()){
+            Entry::where('id', $request->id)->whereHas('sector.user', function($query) use($checkUser){
+                return $query->where('user_id', $checkUser['user']->id);
+            })->delete();
+
+            return response()->json([
+                'success' => true,
+                'errors' => "Congratualtions! Entry Deleted successfully."
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()->all()
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\APIController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('registration', [APIController::class, 'registration']);
+
+Route::post('sectors', [APIController::class, 'sectors']);
+Route::post('create-sector', [APIController::class, 'createSector']);
+Route::post('update-sector', [APIController::class, 'updateSector']);
+Route::post('delete-sector', [APIController::class, 'deleteSector']);
+
+Route::post('budgets', [APIController::class, 'budgets']);
+Route::post('create-budget', [APIController::class, 'createBudget']);
+Route::post('update-budget', [APIController::class, 'updateBudget']);
+Route::post('delete-budget', [APIController::class, 'deleteBudget']);
+
+Route::post('entries', [APIController::class, 'entries']);
+Route::post('create-entry', [APIController::class, 'createEntry']);
+Route::post('update-entry', [APIController::class, 'updateEntry']);
+Route::post('delete-entry', [APIController::class, 'deleteEntry']);
